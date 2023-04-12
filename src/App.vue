0735742bfb9ef59cdcdf4de426fd58a177edc224
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-12 15:31:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data(){
+      return {
+        message: 'Hello Vue',
+        greetingsList: ['Ciao Vue', 'Bonjour Vue','Hola Vue', 'Hallo Vue', 'Olà Vue']
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <ul>
+    <li v-for="greeting in greetingsList">{{ greeting }}</li>
+  </ul>
+</template>
+
+<style></style>
